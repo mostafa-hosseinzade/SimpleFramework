9fb7ab813fa18d09d2a445f1a9a7bcf4ee2cf1c6
mostafa321 <mh551139@gmail.com>
2016-06-05 09:12:30 +0430

    Set Character set to utf8 in header and queries

--- lib/Base/DB.php
+++ lib/Base/DB.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace lib\Base;
+
 use lib\DBModel;
 use lib\DBTable;
 
@@ -10,6 +11,7 @@ use lib\DBTable;
  * @author Mr.Mostafa Hosseinzade
  */
 class DataBase {
+
     private $DB_HOST = DB_HOST;
     private $DB_USERNAME = DB_USER_NAME;
     private $DB_PASSWORD = DB_USER_PASSWORD;
@@ -28,7 +30,9 @@ class DataBase {
                     ":host=" . $this->DB_HOST .
                     ";port=" . $this->DB_PORT .
                     ";charset=utf8" .
-                    ";dbname=" . $this->DB_NAME, $this->DB_USERNAME, $this->DB_PASSWORD);
+                    ";dbname=" . $this->DB_NAME, $this->DB_USERNAME, $this->DB_PASSWORD, array(
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+            ));
             $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
             return 'Can,t Connect to database' . $e->getMessage();
@@ -41,7 +45,7 @@ class DataBase {
     public function CloseLink() {
         $this->pdo = NULL;
     }
-    
+
     /**
      * @param string $model
      * @return \lib\DBModel
@@ -49,12 +53,13 @@ class DataBase {
     public function getModel($model) {
         return new DBModel($model);
     }
-    
+
     /**
      * @param string $table
      * @return \lib\DBTable
      */
-    public function getTable($table){
+    public function getTable($table) {
         return new DBTable($table);
     }
+
 }
--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+/* Set internal character encoding to UTF-8 */
+header('Content-Type: text/html; charset=utf-8');
 require_once __DIR__ . '/../app/autoload.php';
 require_once __DIR__.'/../admin/autoload.php';
 
